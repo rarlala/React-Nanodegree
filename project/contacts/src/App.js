9a8211dce72f3455361766da8e618eedf8207b1b
JEONGSUJI <wtw9611@gmail.com>
2020-01-25 01:27:38 +0900

    Edit App.js / contacts code

--- project/contacts/src/App.js
+++ project/contacts/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ContactList extends React.Component{
+  render() {
+    const people = this.props.contacts
+
+    return <ol>
+      {people.map((person)=>(
+        <li key={person.name}>{person.name}</li>
+      ))}
+    </ol>
+  }
+}
+
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <ContactList contacts= {[
+          { name: 'Tyler' },
+          { name: 'Karen' },
+          { name: 'Richard' }
+        ]}/>
+        <ContactList contacts= {[
+          { name: 'Amanda' },
+          { name: 'Mikenzi' },
+          { name: 'Ryan' }
+        ]}/>
+      </div>
+    );
+  }
 }
 
 export default App;
